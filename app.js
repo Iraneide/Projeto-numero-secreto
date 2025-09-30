52dbe52b5a7f34c3e8fc779b250137f63da96b3a
Iraneide <iraneide.ti@gmail.com>
2025-09-30 14:10:20 -0300

    atualizando numero

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 alert('Boas Vindas ao jogo numero secreto!');
+let numeroSecreto = 5;
+console.log(numeroSecreto)
+let chute = prompt("Escolha um numero entre 1 e 10")
+
+// Se o chute for igual ao numero secreto 
+if (chute == numeroSecreto){
+    console.log('Isso ai! Você descobiu o numer secreto (5)');
+}else{
+    alert('Você errou!')
+}
